b6dd7aef63a5a4584f85a46f0cb5c35d5363d215
CannedOrgi <114853288+CannedOrgi@users.noreply.github.com>
2026-02-16 22:52:21 -0800

    Update NNpricing.R

--- R/NNpricing.R
+++ R/NNpricing.R
@@ -37,7 +37,7 @@ EvaluateNeuralNetwork <- function(nn,x){
   val <- (nn$weights[[nLayers]]%*%val + nn$biases[[nLayers]])*nn$scaleStdOut + nn$scaleMeanOut
   return(val)
 }
-EvaluateModelInGri <- function(model,x){
+EvaluateModelInGrid <- function(model,x){
 # Description: Evaluates a model in the fixed contract grid.
   
   # Check bounds:
